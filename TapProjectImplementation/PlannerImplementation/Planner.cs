d8a3e9fbded39e46ab3fb620e2fd9cb4296b6030
alera <alera@DESKTOP-J73ISDR>
2018-08-26 21:01:50 +0200

    FindTrip method has been implemented

--- TapProjectImplementation/PlannerImplementation/Planner.cs
+++ TapProjectImplementation/PlannerImplementation/Planner.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using PlannerTest;
 using TAP2017_2018_PlannerInterface;
 using TAP2017_2018_TravelCompanyInterface;
 using TAP2017_2018_TravelCompanyInterface.Exceptions;
 
 namespace PlannerImplementation
 {
+
     public class Planner : IPlanner
     {
         private HashSet<IReadOnlyTravelCompany> TravelCompanySet = new HashSet<IReadOnlyTravelCompany>();
@@ -42,8 +42,88 @@ namespace PlannerImplementation
 
         public ITrip FindTrip(string source, string destination, FindOptions options, TransportType allowedTransportTypes)
         {
+            Dictionary<string, Tuple<int, ILegDTO>> CityNodesDictionary = new Dictionary<string, Tuple<int, ILegDTO>>();
+            Dictionary<string, int> CityNodeNOT_Visited = new Dictionary<string, int>();
+            HashSet<string> CityNodeVisited = new HashSet<string>();
+            CityNodesDictionary.Add(source,new Tuple<int, ILegDTO>(0,null));
+            CityNodeNOT_Visited.Add(source, 0);
             
-            throw new NotImplementedException();
+
+            while (CityNodeNOT_Visited.Any())
+            {
+                var cityVisiting = CityNodeNOT_Visited.Aggregate((l, r) => l.Value < r.Value ? l : r); 
+                CityNodeNOT_Visited.Remove(cityVisiting.Key);
+                CityNodeVisited.Add(cityVisiting.Key);
+
+                if (cityVisiting.Key == destination) 
+                    return GetBestTrip(CityNodesDictionary, source, destination);
+
+                int costToReachCity = cityVisiting.Value; 
+                foreach (var TravelCompany in TravelCompanySet)
+                {
+                    var legsOfTravelCompanyFromCity = TravelCompany.FindDepartures(cityVisiting.Key, allowedTransportTypes);
+                    foreach (var leg in legsOfTravelCompanyFromCity)
+                    {
+                        if (!CityNodeVisited.Contains(leg.To))
+                        {
+                            int legCost = 0;
+                            switch (options)
+                            {
+                                case FindOptions.MinimumDistance:
+                                    legCost = leg.Distance;
+                                    break;
+
+                                case FindOptions.MinimumCost:
+                                    legCost = leg.Cost;
+                                    break;
+
+                                case FindOptions.MinimumHops:
+                                    legCost++;
+                                    break;
+                            }
+                            if(CityNodesDictionary.ContainsKey(leg.To))
+                            {
+                                
+                                if ((costToReachCity + legCost) < CityNodesDictionary[leg.To].Item1)
+                                {  
+                                    CityNodesDictionary.Remove(leg.To);
+                                    CityNodesDictionary.Add(leg.To,new Tuple<int, ILegDTO>(costToReachCity+legCost,leg));
+                                    CityNodeNOT_Visited[leg.To] = costToReachCity + legCost;
+                                }
+
+                            }
+                            else
+                            {
+                                CityNodesDictionary.Add(leg.To,new Tuple<int, ILegDTO>(costToReachCity+legCost,leg));
+                                CityNodeNOT_Visited.Add(leg.To, costToReachCity + legCost);
+                            }
+                        }
+
+                            
+                    }
+                }
+            }
+            return null; 
+        }
+
+        private ITrip GetBestTrip(Dictionary<string, Tuple<int, ILegDTO>> CityNodesDictionary, string sourceName, string destinationName)
+        {
+            List <ILegDTO> finalPath= new List<ILegDTO>();
+            int totalCost=0, totalDistance=0;
+            string cursorCityName = destinationName;
+            while (!(CityNodesDictionary[cursorCityName].Item2 is null))
+            {
+
+                finalPath.Add(CityNodesDictionary[cursorCityName].Item2);
+                totalCost += CityNodesDictionary[cursorCityName].Item2.Cost;
+                totalDistance += CityNodesDictionary[cursorCityName].Item2.Distance;
+                cursorCityName = CityNodesDictionary[cursorCityName].Item2.From;
+            }
+
+            finalPath.Reverse();
+            return new Trip(sourceName, destinationName, finalPath.AsReadOnly(), totalCost, totalDistance);
+            
         }
     }
+
 }
--- TapProjectImplementation/PlannerImplementation/Trip.cs
+++ TapProjectImplementation/PlannerImplementation/Trip.cs
@@ -7,10 +7,19 @@ using System.Threading.Tasks;
 using TAP2017_2018_PlannerInterface;
 using TAP2017_2018_TravelCompanyInterface;
 
-namespace PlannerImplementation
+namespace PlannerTest
 {
     class Trip : ITrip
     {
+        public Trip(string from, string to, ReadOnlyCollection<ILegDTO> path, int totalCost, int totalDistance)
+        {
+            From = from;
+            To = to;
+            Path = path;
+            TotalCost = totalCost;
+            TotalDistance = totalDistance;
+        }
+
         public string From { get; }
         public string To { get; }
         public ReadOnlyCollection<ILegDTO> Path { get; }
